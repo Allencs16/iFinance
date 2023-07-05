4b4ec9062f95474af49d5d48e115b8e3d0eecc38
Breno AllenCS <brenoallencs@gmail.com>
2023-07-04 21:45:12 -0300

    Initial Commit

---
+++ IFinance/IFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  IFinanceApp.swift
+//  IFinance
+//
+//  Created by Breno Alencar on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct IFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
