3aa620ca2936c96519441ce005efaeeb65628f2d
Breno AllenCS <brenoallencs@gmail.com>
2023-07-06 15:11:06 -0300

    Using state for variable and Testing layouts

--- IFinance/ContentView.swift
+++ IFinance/ContentView.swift
@@ -6,18 +6,40 @@
 //
 
 import SwiftUI
-var number: Int = 0
 
 struct ContentView: View {
+    @State var firstNumber: Int = 0;
     var body: some View {
         VStack{
             NavigationSplitView {
                 Text("IFinance").foregroundColor(.primary)
-                Button("Settings"){
-                    SettingsView()
+                Button(action: {
+                    firstNumber += 1
+                    print("Teste \(firstNumber)")
+                }){
+                    Text("Settings")
                 }
             } detail: {
-                /*@START_MENU_TOKEN@*/Text("Detail")/*@END_MENU_TOKEN@*/
+                VStack{
+                    HStack{
+                        VStack{
+                            Text("Dinheiro disponivel").font(.title2)
+                            Text("$\(firstNumber)").font(.title2)
+                        }.padding(.all)
+                            .background(Color.white)
+                            .opacity(0.4)
+                            .cornerRadius(20)
+                            .shadow(radius: 5)
+                        VStack{
+                            Text("Dinheiro disponivel").font(.title2)
+                            Text("$\(firstNumber)").font(.title2)
+                        }.padding(.all)
+                            .background(Color.white)
+                            .opacity(0.4)
+                            .cornerRadius(20)
+                            .shadow(radius: 5)
+                    }
+                }
             }
         }
     }
